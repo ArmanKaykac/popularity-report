692080c8fbb44fb19372826b64a8bfa807600c7c
ArmanKaykac <35624921+ArmanKaykac@users.noreply.github.com>
2023-03-05 18:08:32 +0300

    image model, controller, repository, service

---
+++ report/src/main/java/com/popularity/report/service/ImageDataService.java
@@ -0,0 +1,51 @@
+package com.popularity.report.service;
+
+import com.popularity.report.model.ImageData;
+import com.popularity.report.repository.ImageDataRepository;
+import com.popularity.report.util.ImageUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.transaction.Transactional;
+import java.io.IOException;
+import java.util.Optional;
+
+@Service
+public class ImageDataService {
+
+    @Autowired
+    private ImageDataRepository imageDataRepository;
+
+    public String uploadImage(MultipartFile file) throws IOException {
+
+        imageDataRepository.save(ImageData.builder()
+                .name(file.getOriginalFilename())
+                .type(file.getContentType())
+                .imageData(ImageUtil.compressImage(file.getBytes())).build());
+
+        return ("Image uploaded successfully: " +
+                file.getOriginalFilename());
+
+    }
+
+    @Transactional
+    public ImageData getInfoByImageByName(String name) {
+        Optional<ImageData> dbImage = imageDataRepository.findByName(name);
+
+        return ImageData.builder()
+                .name(dbImage.get().getName())
+                .type(dbImage.get().getType())
+                .imageData(ImageUtil.decompressImage(dbImage.get().getImageData())).build();
+
+    }
+
+    @Transactional
+    public byte[] getImage(String name) {
+        Optional<ImageData> dbImage = imageDataRepository.findByName(name);
+        byte[] image = ImageUtil.decompressImage(dbImage.get().getImageData());
+        return image;
+    }
+
+
+}
